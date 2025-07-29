36d832ac37fbdb38f16f1ba3a7974179c0564f35
Ramon Colmenares <86928180+RamonColmenares@users.noreply.github.com>
2025-07-29 08:33:28 -0500

    Configure frontend for Vercel deployment

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,9 +14,7 @@ const config = {
   ],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    // adapter-vercel optimizes the app for deployment to Vercel
     adapter: adapter(),
   },
 };
